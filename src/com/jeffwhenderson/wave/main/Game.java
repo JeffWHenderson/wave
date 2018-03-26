6b122a28b513c7bb3984497631e82ca96befdb80
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-03-26 16:38:28 -0400

    added a start and stop function. added a Single thread for the game.

--- src/com/jeffwhenderson/wave/main/Game.java
+++ src/com/jeffwhenderson/wave/main/Game.java
@@ -6,13 +6,30 @@ public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 	
 	public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9; 
+	private Thread thread;
+	private boolean running = false; // needs a getter and setter possibly
 	
 	public Game() {
 		new Window(WIDTH, HEIGHT, "PLACEHOLDER!! in Game constructor", this);
 	}
 
 	public synchronized void start() {
-		
+		thread = new Thread(this);
+		thread.start();
+		running = true;
+	}
+	
+	public synchronized void stop() {
+		try 
+		{
+			thread.join();
+			running = false;
+		} 
+		catch(Exception e) 
+		{
+			e.printStackTrace();
+		}
+
 	}
 	
 	@Override
