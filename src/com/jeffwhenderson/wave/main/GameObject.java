892fd003f24b770170cef2125b54f45656288fa7
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-03-27 15:49:49 -0400

    added getters and setters to the game object.

--- src/com/jeffwhenderson/wave/main/GameObject.java
+++ src/com/jeffwhenderson/wave/main/GameObject.java
@@ -15,4 +15,39 @@ public abstract class GameObject {
 	
 	public abstract void tick();
 	public abstract void render(Graphics g);
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setId(ID id) {
+		this.id = id;
+	}
+	public ID getId() {
+		return id;
+	}
+	
+	public void setVelX(int velX) {
+		this.velX = velX;
+	}
+	public void setVelY(int velY) {
+		this.velY = velY;
+	}
+	
+	public int getVelY() {
+		return velY;
+	}
+	public int getVelX() {
+		return velX;
+	}
+	
+	
 }
