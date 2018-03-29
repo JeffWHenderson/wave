c689ccf058e1fd1657c4ee5e9b0552c3a0d2633f
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-03-28 20:42:00 -0400

    added a trail Class. I'm going to whole foods now to get some awesome food to cook this week.

---
+++ src/com/jeffwhenderson/wave/main/Trail.java
@@ -0,0 +1,31 @@
+package com.jeffwhenderson.wave.main;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Trail extends GameObject {
+
+	public Trail(int x, int y, ID id) {
+		super(x, y, id);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Rectangle getBounds() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
